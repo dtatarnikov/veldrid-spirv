4cb1848d908f1fd6359789d9d1c5bd8328efe157
Denis Tatarnikov <strong2much@gmail.com>
2026-02-21 13:47:02 +0700

    Upgrade Maccatalyst build

--- build-native.sh
+++ build-native.sh
@@ -2,12 +2,8 @@
 
 scriptPath="$( cd "$(dirname "$0")" ; pwd -P )"
 _CMakeBuildType=Debug
-_CMakeEnableBitcode=
-_OutputPathPrefix=
 _CMakeBuildTarget=veldrid-spirv
 _CMakeOsxArchitectures=
-_CMakeGenerator=
-_CMakeExtraBuildArgs=
 _OSDir=
 
 while :; do
@@ -35,10 +31,6 @@ while :; do
             _OSDir=linux-arm64
             ;;
         ios)
-            _CMakeEnableBitcode=-DENABLE_BITCODE=0
-            _CMakeBuildTarget=veldrid-spirv
-            _CMakeGenerator="-G Xcode"
-            _CMakeExtraBuildArgs="--config Release"
             _OSDir=ios
             ;;
         *)
@@ -59,54 +51,43 @@ mkdir -p $_OutputPath
 pushd $_OutputPath
 
 if [[ $_OSDir == "ios" ]]; then
+	_CMakeEnableBitcode=-DENABLE_BITCODE=0
+	_CMakeGenerator="-G Xcode"
+	_CMakeExtraBuildArgs="--config Release"
+	_CMakeToolchain=../../ios/ios.toolchain.cmake
+
 	# Build for iOS device (arm64)
     mkdir -p device-build
     pushd device-build
 
-    cmake ../../../.. -DIOS=ON -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator -DPLATFORM=OS64 -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures"
+    cmake ../../../.. $_CMakeGenerator -DCMAKE_BUILD_TYPE=$_CMakeBuildType -DCMAKE_TOOLCHAIN_FILE=$_CMakeToolchain -DPLATFORM=OS64 -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath
     cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs
 
     popd
 
 	# Build for iOS simulator (combined arm64 + x86_64)
-    mkdir -p simulator-build-arm64
-    pushd simulator-build-arm64
+    mkdir -p simulator-build
+    pushd simulator-build
 
-    cmake ../../../.. -DIOS=ON -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator -DPLATFORM=SIMULATORARM64 -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures"
+    cmake ../../../.. $_CMakeGenerator -DCMAKE_BUILD_TYPE=$_CMakeBuildType -DCMAKE_TOOLCHAIN_FILE=$_CMakeToolchain -DPLATFORM=SIMULATOR64COMBINED -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath
     cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs
 
     popd
-
-    mkdir -p simulator-build-x64
-    pushd simulator-build-x64
-
-    cmake ../../../.. -DIOS=ON -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator -DPLATFORM=SIMULATOR64 -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures"
-    cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs
-
-    popd
-
-    mkdir -p simulator-build-combined/veldrid-spirv.framework
-
-    cp ./simulator-build-arm64/Release-iphonesimulator/veldrid-spirv.framework/Info.plist ./simulator-build-combined/veldrid-spirv.framework/Info.plist
-
-    lipo -create \
-	    ./simulator-build-arm64/Release-iphonesimulator/veldrid-spirv.framework/veldrid-spirv \
-	    ./simulator-build-x64/Release-iphonesimulator/veldrid-spirv.framework/veldrid-spirv \
-	 -output ./simulator-build-combined/veldrid-spirv.framework/veldrid-spirv
 
 	# Build for Mac Catalyst (universal arm64 + x86_64)
     mkdir -p maccatalyst-build
     pushd maccatalyst-build
 
-    cmake ../../../.. -DIOS=ON -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator -DPLATFORM=MAC_CATALYST -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
-    cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs
+	cmake ../../../.. $_CMakeGenerator -DCMAKE_BUILD_TYPE=$_CMakeBuildType -DCMAKE_TOOLCHAIN_FILE=$_CMakeToolchain -DPLATFORM=MAC_CATALYST_UNIVERSAL -DDEPLOYMENT_TARGET=13.4 $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath
+    #cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs	
+	xcodebuild -project $_CMakeBuildTarget.xcodeproj -scheme $_CMakeBuildTarget -configuration Release -destination 'generic/platform=iOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
 
     popd
 
 	# Final combine all builds to framework
     xcodebuild -create-xcframework \
 	    -framework ./device-build/Release-iphoneos/veldrid-spirv.framework \
-	    -framework ./simulator-build-combined/veldrid-spirv.framework \
+	    -framework ./simulator-build/Release-iphonesimulator/veldrid-spirv.framework \
         -framework ./maccatalyst-build/Release/veldrid-spirv.framework \
 	    -output ./veldrid-spirv.xcframework
 else
