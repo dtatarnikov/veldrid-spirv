f33439d25c5365e126618d601ad41face13b8ec8
Denis Tatarnikov <strong2much@gmail.com>
2026-05-19 19:27:07 +0700

    Support 16KB page size for android

--- build-native.cmd
+++ build-native.cmd
@@ -26,6 +26,9 @@ goto ArgLoop
 :LocateVS
 
 set _CMAKE_ARGS=-DCMAKE_BUILD_TYPE=%_CMAKE_BUILD_TYPE%
+if defined _ANDROID_ABI (
+  set _CMAKE_ARGS=%_CMAKE_ARGS% -DANDROID=ON
+)
 if defined _NDK_DIR (
   set _CMAKE_ARGS=%_CMAKE_ARGS% -G "MinGW Makefiles" -DANDROID_ABI=%_ANDROID_ABI% -DANDROID_PLATFORM=%_ANDROID_PLATFORM% -DCMAKE_MAKE_PROGRAM="%_NDK_DIR%\prebuilt\windows-x86_64\bin\make.exe" -DCMAKE_TOOLCHAIN_FILE="%_NDK_DIR%\build\cmake\android.toolchain.cmake" -DCMAKE_CXX_FLAGS_RELEASE=-g0
   set _OS_DIR=android
